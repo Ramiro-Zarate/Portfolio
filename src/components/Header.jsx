0efd3ddf05c524685d496fa0163b2846add81ba1
Ramiro-Zarate <ramazara13@gmail.com>
2026-05-13 14:56:44 -0300

    feat: Add Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,16 @@ import { Link } from 'react-router'
 export function Header() {
     return (
         <header>
-            <h1>Ramiro Zarate</h1>
-            <nav>
-                <Link to="/">Inicio</Link>
-                <Link to="/tech">Tecnologias</Link>
-                <Link to="/projects">Proyectos</Link>
-                <Link to="/contact">Contacto</Link>
-            </nav>
+            <div className={styles.headerContainer}>
+                <h1>Ramiro Zarate</h1>
+                <nav className={styles.nav}>
+                    <Link className={styles.link} to="/">Inicio</Link>
+                    <Link className={styles.link} to="/tech">Tecnologias</Link>
+                    <Link className={styles.link} to="/projects">Proyectos</Link>
+                    <Link className={styles.link} to="/contact">Contacto</Link>
+                </nav>
+                <button className={styles.downloadBtn}>Descargar CV</button>
+            </div>
         </header>
     )
 }
